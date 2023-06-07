db2fc026b74dea289afb07a9b4bfe9ecf6dcdb72
oolidu <arquitecturaod@gmail.com>
2023-06-06 21:56:01 -0400

    avance metodos analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,14 +1,20 @@
 const analyzer = {  
   getWordCount: (text) => {
+    const palabras = text.split (' ');
+    return palabras.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+    return text.length
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    const sinespaciosText = text.replace(/\W/g, '').trim();
+    return sinespaciosText.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => { 
+    //reutilizar trim, split (' '), lenth,    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
