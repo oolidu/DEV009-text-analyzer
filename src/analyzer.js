b0d252eacc27b17c3423822881a6bea559697f13
oolidu <arquitecturaod@gmail.com>
2023-06-12 19:27:27 -0400

    terminé funciones, html y css

--- src/analyzer.js
+++ src/analyzer.js
@@ -4,25 +4,54 @@ const analyzer = {
     return palabras.length;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getCharacterCount: (text) => {
     return text.length
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     const sinespaciosText = text.replace(/\W/g, '').trim();
     return sinespaciosText.length;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getAverageWordLength: (text) => { 
+    const palabras = text.split (' ');
+    const totalPalabras = palabras.length;
+
+    let longitudTotal = 0;
+    for (let i = 0; i< totalPalabras; i++) {
+      longitudTotal += palabras[i].trim().length;
+    }
+
+    const longitudMedia = longitudTotal/totalPalabras;
+    return longitudMedia; 
     //reutilizar trim, split (' '), lenth,    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getNumberCount: (text) => {
+
+  getNumberCount: (text)  => {
+    const numeros = text.match (/\d+/g);
+    if (numeros) {
+      return numeros.length;    }
+    else {
+      return 0;
+      } 
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numeros = text.match (/\d+/g);
+    if (numeros) {
+      const sum = numeros.reduce((acc,num)=> acc + Number(num), 0);
+      return sum;}
+      else {
+        return 0;
+      } 
+        //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+  
 };
 
 export default analyzer;
