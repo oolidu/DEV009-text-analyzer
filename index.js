6509aed9ceebe067237b5dfb6c906d7a93e62d6c
oolidu <arquitecturaod@gmail.com>
2023-06-14 20:49:13 -0400

    Updates

---
+++ index.js
@@ -0,0 +1,71 @@
+import analyzer from './analyzer.js';
+
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+//Selecciono elementos
+const textarea = document.querySelector('textarea[name="user-input"]');
+
+const getWordCount = document.querySelector(".caja-verde:nth-child(3)");
+const getCharacterCount = document.querySelector(".caja-verde:nth-child(1)");
+const getCharacterCountExcludingSpaces = document.querySelector(".caja-verde:nth-child(2)");
+const getAverageWordLength = document.querySelector(".caja-verde:nth-child(6)");
+const getNumberCount = document.querySelector(".caja-verde:nth-child(4)");
+const getNumberSum = document.querySelector(".caja-verde:nth-child(5)"); 
+
+const button = document.getElementById("reset-button");
+
+//Agrego listeners
+
+button.addEventListener('click', () => {
+  limpiar();
+  resetearBotones();
+});
+
+function resetearBotones() {
+  const botones= document.querySelectorAll('.caja-verde');
+  botones.forEach((boton) => {
+    boton.textContent = ' ';
+    
+  });
+}
+
+function limpiar() {
+    textarea.value = "";
+    textarea.placeholder = "Escribe o pega un texto aquí...";
+}
+
+textarea.addEventListener('keyup', (event) => {
+    const texWordCount = event.target.value;
+    const wordCount = analyzer.getWordCount(texWordCount);
+    getWordCount.textContent = wordCount;
+  });
+
+  textarea.addEventListener('keyup', (event) => {
+    const texCharacterCount = event.target.value;
+    const characterCount = analyzer.getCharacterCount(texCharacterCount);
+    getCharacterCount.textContent = characterCount;
+  });
+
+  textarea.addEventListener('keyup', (event) => {
+    const texCharacterCountExcludingSpaces = event.target.value;
+    const characterCountExcludingSpaces = analyzer.getCharacterCountExcludingSpaces(texCharacterCountExcludingSpaces);
+    getCharacterCountExcludingSpaces.textContent = characterCountExcludingSpaces;
+  });
+
+  textarea.addEventListener('keyup', (event) => {
+    const texAverageWordLength = event.target.value;
+    const averageWordLength = analyzer.getAverageWordLength(texAverageWordLength);
+    getAverageWordLength.textContent = averageWordLength;
+  });
+
+  textarea.addEventListener('keyup', (event) => {
+    const texNumberCount = event.target.value;
+    const numberCount = analyzer.getNumberCount(texNumberCount);
+    getNumberCount.textContent = numberCount;
+  });
+
+  textarea.addEventListener('keyup', (event) => {
+    const texNumberSum = event.target.value;
+    const numberSum = analyzer.getNumberSum(texNumberSum);
+    getNumberSum.textContent = numberSum;
+  });
